b1ad60dfa5e11dd23580e673fb8d1d89e2020a42
KerolosYoussef <kerolosyoussef2021@gmail.com>
2025-08-03 14:16:03 +0300

    add: Domain Models

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -9,5 +9,25 @@
         public string Country { get; } = default!;
         public string State { get; } = default!;
         public string ZipCode { get; } = default!;
+
+        protected Address() { }
+
+        private Address(string firstName, string lastName, string email, string addressLine, string country, string state, string zipCode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            AddressLine = addressLine;
+            Country = country;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public static Address Of(string firstName, string lastName, string email, string addressLine, string country, string state, string zipCode)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(email, nameof(email));
+            ArgumentException.ThrowIfNullOrWhiteSpace(addressLine, nameof(addressLine));
+            return new(firstName, lastName, email, addressLine, country, state, zipCode);
+        }
     }
 }
